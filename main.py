676f717abd261590c4b310b253ba10ed1f896ded
Tobby Echonga <30405173+TobbyEchonga@users.noreply.github.com>
2021-10-10 01:32:03 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+
+# Python3 code to determine Length of
+# longest consecutive zeroes in the
+# binary representation of a number.
+def maxZeros(N):
+ 
+    # variable to store the length
+    # of longest consecutive 0's
+    maxm = -1
+ 
+    # to temporary store the
+    # consecutive 0's
+    cnt = 0
+    while(N):
+        if(not(N & 1)):
+            cnt += 1
+            N >>= 1
+            maxm = max(maxm,cnt)
+        else:
+            maxm = max(maxm,cnt)
+            cnt = 0
+            N >>= 1
+ 
+    return maxm
+ 
+# Driver Code
+N = 14
+print(maxZeros(N))
+# Output : 1
+# Binary representation of 14 is 1110. There is only one 0 in the binary representation.
